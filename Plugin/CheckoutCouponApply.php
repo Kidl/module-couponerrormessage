3a4b371d30e72294af0e19ec98fd48c48ff73267
olegfod <of@kidl.no>
2021-03-13 11:23:21 +0100

    first commit

---
+++ Plugin/CheckoutCouponApply.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Kidl\CouponErrorMessage\Plugin;
+
+use Kidl\CouponErrorMessage\Helper\Data as ConfigData;
+use Kidl\CouponErrorMessage\Helper\Validator as CouponValidator;
+use Magento\Framework\Exception\LocalizedException;
+
+class CheckoutCouponApply
+{
+    /**
+     * Module Helper
+     *
+     * @var \Kidl\CouponErrorMessage\Helper\Validator
+     */
+
+    protected $_couponValidator;
+    /**
+     * Module helper
+     *
+     * @var \Kidl\CouponErrorMessage\Helper\Data
+     */
+    protected $_configData;
+
+    public function __construct(
+        CouponValidator $couponValidator,
+        ConfigData $configData
+    ) {
+        $this->_couponValidator = $couponValidator;
+        $this->_configData = $configData;
+    }
+
+    /**
+     * This function runs around the set function for coupon api, once the core function throws
+     * exception this function will catch and update the error message.
+     *
+     * @param \Magento\Quote\Model\CouponManagement $subject
+     * @param callable $proceed
+     * @param int $cartId
+     * @param string $couponCode
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function aroundSet(\Magento\Quote\Model\CouponManagement $subject, callable $proceed, $cartId, $couponCode)
+    {
+        $result = false;
+
+        try {
+            $result = $proceed($cartId, $couponCode);
+        } catch (\Exception $e) {
+            if ($this->_configData->isEnabled()) {
+                $msg = $this->_couponValidator->validate($couponCode);
+                if (!empty($msg)) {
+                    throw new LocalizedException(__("%l", $msg));
+                }
+            }
+            throw $e;
+        }
+
+        return $result;
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Coupon Error Message
+The default message is not self-explanatory which explains the reason for coupon failure. Coupon Error Message extension for Magento 2 allows an admin to specify an error message for each condition of the coupon shopping cart rules. When the condition is not passed through validation, an appropriate error message will be shown to a customer which will be configured in the backend. 
+
+## Feature Summary
+- Error message when coupon does not exist.  
+- Error message when coupon exists but is expired.  
+- Error message when a customer doesn't belong to the assigned customer group.  
+- Error message when the coupon is not applicable to the website.
+- Error message when the coupon was used more than it can be used.  
+- Error messages when coupon exists but do not apply to the cart rule conditions.  
